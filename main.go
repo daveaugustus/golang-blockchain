5dea9ba6ea8c3e453428c7e7c0dc8acb85b1e542
Dave Augustus <davetweetlive@gmail.com>
2024-08-21 01:37:28 +0530

    Added proof of work and validation

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
 
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
+	"github.com/daveaugustus/golang-blockchain/blockchain"
+)
 
 func main() {
-	fmt.Println("Starting")
+	chain := blockchain.InitBlockChain()
+	chain.AddBlock("First block after genesis block")
+	chain.AddBlock("Second block after genesis block")
+	chain.AddBlock("Third block after genesis block")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
 }
